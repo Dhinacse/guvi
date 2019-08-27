d58cdd10d446d55b167838a3d8a54b656e5d7218
dhina <46160908+Dhinacse@users.noreply.github.com>
2019-08-27 11:56:47 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+student ={'ram': 23,'ravi': 23}
+for name, age in student.items():
+    print( name +' ' ' is' ' '+ str(age) +' ''years old')
+
+student ={'ram': 23,'ravi': 23}
+for name in student.keys():
+    print( 'My name is '+ name)
+    
+student ={'ram': 23,'ravi': 23}
+for age in student.values():
+    print( str(age) +' ''years old')
